28868745a762fd7d2ca11824018d10ce765b19e3
sarthak <sarthakchatur0@gmail.com>
2024-07-31 01:01:56 +0530

    movement and animation added

---
+++ UnityDeveloper_Test/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   
+    public float speed = 5f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-moveHorizontal, 0.0f, -moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
